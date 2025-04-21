9c2e56ac008c2e91f83e3bfc5bdc5794b4c702b1
VladimirVladimirovMinkov <Vladimir.Vladimirov.Minkov@gmail.com>
2025-04-21 14:25:13 +1200

    note
    Just adding a note

--- src/ESP32 test code 1/servo/servo.ino
+++ src/ESP32 test code 1/servo/servo.ino
@@ -1,60 +1,57 @@
 void setup() {
-
   Serial.begin(115200);
 
-  pinMode(2, OUTPUT);
+  pinMode(14, INPUT);
+  pinMode(27, INPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
   delay(100);
-  digitalWrite(2, LOW);
-  delay(100);
-
-  pinMode(15, OUTPUT);
-  delay(100);
-  digitalWrite(15, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(13, LOW);
   delay(100);
 }
 
 long start;
-char snum[5];
-int i = 1000;
-bool increasingi = true;
-int j = 1000;
-bool increasingj = true;
+int rudder = 1000;
+int elevator = 1000;
+int events[12];
 
 void loop() {
-
   while(Serial.available()>0)
   {
-    if(Serial.read() == 's')
+    if(Serial.peek() == 's')
     {
-      i = 1000;
-      j = 1000;
+      rudder = 1000;
+      elevator = 1000;
       delay(1000);
     }
+    Serial.write(Serial.read());
   }
 
 
-  if(i > 2500)
-    increasingi = false;
-  else if(i < 500)
-    increasingi = true;
-  
-  if(j > 2500)
-    increasingj = false;
-  else if(j < 500)
-    increasingj = true;
-  
-  i = i + (increasingi - !increasingi);
-  j = j + (increasingj - !increasingj);
+  start = micros();
+  digitalWrite(13, HIGH);
+  while (micros() - start < rudder);
+  digitalWrite(13, LOW);
 
   start = micros();
-  digitalWrite(15, HIGH);
-  while (micros() - start < i);
-  digitalWrite(15, LOW);
+  digitalWrite(12, HIGH);
+  while (micros() - start < elevator);
+  digitalWrite(12, LOW);
 
-  start = micros();
-  digitalWrite(2, HIGH);
-  while (micros() - start < j);
-  digitalWrite(2, LOW);
+  for(int i = 0; i < 5; i++)
+  {
+    start = micros();
+    rudder = (analogRead(14)/3)+500;
+    elevator = (analogRead(27)/3)+500;
 
-  delay(7);
+    events[i*2] =  rudder;
+    events[i*2+1] = elevator;
+    // 10 milisconds of code
+
+    while (micros() - start < 10000);
+  }
+  events[10] = random(200); // heart rate
+  events[11] = micros();
+  // save the events to eeprom or sd
 }
